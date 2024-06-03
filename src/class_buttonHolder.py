d76dd590ba69393c6fcb283fec6050cfda6eb3ea
Rodrigo Parrilla Mesas <rodrigoparri@gmail.com>
2024-06-03 12:16:36 +0200

    simple window with pushbutton

---
+++ src/class_buttonHolder.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QMainWindow, QPushButton
+
+class ButtonHolder(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        button = QPushButton("Press")
+        self.setCentralWidget(button)
+        #self.show()
+
---
+++ src/main.py
@@ -0,0 +1,9 @@
+# two main APIs QT widgets and QML this course is about QT widgets
+from PySide6.QtWidgets import QApplication
+import sys
+from class_buttonHolder import ButtonHolder
+if __name__ == "__main__":
+  app = QApplication(sys.argv)
+  window = ButtonHolder()
+  window.show()
+  app.exec() #stat event loop
